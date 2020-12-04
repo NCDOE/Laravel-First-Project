f83e87fc483bf9f1c44835864e1de896800f086e
Lekhetho L. Kobuoe <LekhethoKobuoe@ncdoe.local>
2020-12-04 12:08:47 +0200

    Landing Page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::get('/RequestQoute', function () {
     return view('RequestQoute');
 });
